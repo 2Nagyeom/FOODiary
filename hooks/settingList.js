e1e6fe7643095c07287d1966ae1b91e863b4ffc8
2Nagyeom <artest1081@gmail.com>
2024-07-18 15:48:50 +0900

    add : geolocation add

--- hooks/settingList.js
+++ hooks/settingList.js
@@ -9,32 +9,25 @@ const settingList = [
         id : 1,
         content : '공지사항',
         flag : 'LINK',
-        link : 'https://invented-zone-8ba.notion.site/a04aa4551221432a8c3655412a84ef4c?pvs=4',
+        link : 'https://invented-zone-8ba.notion.site/0a272d887d50478ab4afec6a58ba4531?pvs=4',
         icon : goInfoIcon
     },
     {
         id : 2,
         content : '자주하는 질문',
         flag : 'LINK',
-        link : 'https://invented-zone-8ba.notion.site/a04aa4551221432a8c3655412a84ef4c?pvs=4',
+        link : 'https://invented-zone-8ba.notion.site/b1a4b512e5c24a0bba612dc80a5c8601?pvs=4',
         icon : goInfoIcon
     },
     {
         id : 3,
-        content : '1 : 1 문의',
-        flag : 'LINK',
-        link : 'https://invented-zone-8ba.notion.site/a04aa4551221432a8c3655412a84ef4c?pvs=4',
-        icon : goInfoIcon
-    },
-    {
-        id : 4,
         content : '이용약관',
         flag : 'LINK',
         link : 'https://invented-zone-8ba.notion.site/a04aa4551221432a8c3655412a84ef4c?pvs=4',
         icon : goInfoIcon
     },
     {
-        id : 5,
+        id : 4,
         content : '앱 버전',
         flag : 'VERSION',
         icon : goInfoIcon
